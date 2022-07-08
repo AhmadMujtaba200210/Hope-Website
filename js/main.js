151b2050cbd65aae615ea36a87f6127eee5c0610
Ahmad Mujtaba <cadeta6@gmail.com>
2022-07-08 19:33:36 +0500

    updated
    this contains no error

--- js/main.js
+++ js/main.js
@@ -1,21 +1,8 @@
 jQuery(document).ready(function(){
     "use strict"
     $('.slider').ripples({
-  dropRadius: 15,
-  perturbance: 0.01,
-  
-});
-    
-    
-     $(".text").typed({
-        
-        strings:["First sentence.","Second sentence."],
-        typespeed:0,
-        loop:true   
-        
-        
+        dropRadius:15,
+        perturbance:0.01
     });
-    
-    
     
 });
