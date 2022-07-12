1697133b85d30678084111955780cb355b35594a
Ahmad Mujtaba <cadeta6@gmail.com>
2022-07-12 18:31:50 +0500

    updated

--- js/main.js
+++ js/main.js
@@ -4,5 +4,18 @@ jQuery(document).ready(function(){
         dropRadius:15,
         perturbance:0.01
     });
-    
+
+   
 });
+
+function changeBg(){
+    var scrollvalue=window.scrollY;
+    var navi=document.getElementById('navi');
+    if(scrollvalue<150){
+        navi.classList.remove('bgColor');
+    }else{
+        navi.classList.add('bgColor');
+    }
+}
+
+window.addEventListener('scroll',changeBg);
